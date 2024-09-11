1f4df8decbd620915192e16e63d8f8e4bc562f5b
musaib64 <syedmusaibshah15@gmail.com>
2024-09-11 15:31:55 +0530

    added new file

---
+++ maxmin.java
@@ -0,0 +1,30 @@
+class  maxmin{
+    //program to print maximum in array
+    public static void main(String[] args)
+    {
+        int[] arr={0,1,2,3};
+        int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>max) {
+                max=arr[i];
+                
+            }
+            
+        }
+        System.out.println("max is :" + max);
+        // program to print minimum in array
+
+        
+        int[] arr2={0,1,2,3};
+        int min=0;
+        for (int i = 0; i < arr2.length; i++) {
+            if (arr2[i]<max) {
+                min=arr2[i];
+                
+            }
+            
+        }
+        System.out.println("min is :" + min);
+        
+    }
+}
---
+++ typecast2.java
@@ -0,0 +1,8 @@
+public class typecast2 {
+    public static void main(String[] args) {
+        short x=132;
+        int y=(~x);
+        System.out.println(y);
+    }
+    
+}
